f81a6540bc33aca3511bc3b0eecb1147a6eabf46
andres <andresbernal@gmail.com>
2022-07-26 17:08:00 -0500

    start demoapp

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Route path='/' />
+      </Router>
+    </>
+  );
+}
+
+export default App;
